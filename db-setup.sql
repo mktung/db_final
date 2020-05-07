39c1d133db788404c307fc3ad134c25c655b3571
Shuting Yang <46876160+yangs21@users.noreply.github.com>
2020-05-07 10:02:38 -0400

    Update db-setup.sql
    Added superuser

--- db-setup.sql
+++ db-setup.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE dbms_final_project;
 CREATE USER dbms_project_user WITH PASSWORD 'dbms_password';
 GRANT ALL PRIVILEGES ON DATABASE dbms_final_project TO dbms_project_user;
+
+ALTER USER dbms_project_user WITH SUPERUSER;
